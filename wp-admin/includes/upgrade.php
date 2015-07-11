91503e85c33a0de5208fc6fed10f9c47c5a1bada
Manuel Canga <dev@trasweb.net>
2015-07-11 13:24:54 +0200

    Upgrades: If a table has already been converted to utf8mb4, there's no need to try and convert it again.

--- wp-admin/includes/upgrade.php
+++ wp-admin/includes/upgrade.php
@@ -903,16 +903,16 @@ function maybe_convert_table_to_utf8mb4( $table ) {
 		return false;
 	}
 
-	foreach ( $results as $column ) {
-		if ( $column->Collation ) {
-			list( $charset ) = explode( '_', $column->Collation );
-			$charset = strtolower( $charset );
-			if ( 'utf8' !== $charset && 'utf8mb4' !== $charset ) {
-				// Don't upgrade tables that have non-utf8 columns.
-				return false;
-			}
-		}
-	}
+    $table_details = $wpdb->get_row( "SHOW TABLE STATUS LIKE '$table'" );
+    if ( ! $table_details ) {
+        return false;
+    }
+
+    list( $table_charset ) = explode( '_', $table_details->Collation );
+    $table_charset = strtolower( $table_charset );
+    if ( 'utf8mb4' === $table_charset ) {
+        return true;
+    }
 
 	return $wpdb->query( "ALTER TABLE $table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci" );
 }
