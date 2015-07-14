9496e3855e7abf6baf1784b0ee06a8f6ef481500
Manuel Canga <dev@trasweb.net>
2015-07-14 11:43:51 +0200

    Shiny Updates: Handle the case where the plugin is installed into a different directory than it previously existed in. A good example of this is when the plugin being updated is currently installed as 'Plugin-Name' but the canonical directory is 'plugin-name', but it can also occur when the plugin is installed in 'super-cool-plugin' and it's canonical name is 'average-plugin'
    Updates: Correctly identify more failed update cases. This checks for a WP_Error being raised during an individual update, in addition to just the bootstrap error cases.
    The error message in these cases is still not displayed in the UI.

--- wp-admin/includes/ajax-actions.php
+++ wp-admin/includes/ajax-actions.php
@@ -2737,7 +2737,10 @@ function wp_ajax_update_plugin() {
 		'oldVersion' => '',
 		'newVersion' => '',
 	);
-	$plugin_data = get_plugin_data( WP_PLUGIN_DIR . '/' . $plugin );
+
+        $plugin_data = get_plugins( '/' . $result[ $plugin ]['destination_name'] ); 
+        $plugin_data = reset( $plugin_data ); 
+
 	if ( $plugin_data['Version'] ) {
 		$status['oldVersion'] = sprintf( __( 'Version %s' ), $plugin_data['Version'] );
 	}
@@ -2756,10 +2759,15 @@ function wp_ajax_update_plugin() {
 		wp_update_plugins();
 	}
 
-	$upgrader = new Plugin_Upgrader( new Automatic_Upgrader_Skin() );
+    $skin = new Automatic_Upgrader_Skin(); 
+    $upgrader = new Plugin_Upgrader( $skin ); 
 	$result = $upgrader->bulk_upgrade( array( $plugin ) );
 
-	if ( is_array( $result ) ) {
+    if ( is_array( $result ) && empty( $result[$plugin] ) && is_wp_error( $skin->result ) ) { 
+        $result = $skin->result; 
+    } 
+
+    if ( is_array( $result ) && !empty( $result[ $plugin ] ) ) { 
 		$plugin_update_data = current( $result );
 
 		/*
